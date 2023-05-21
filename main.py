de6cbf909e558185a1b5ece51c68d685c096d1ab
Filip Nilsson <filip.c.nilsson@gmail.com>
2023-05-21 14:41:18 +0000

    new starting template

--- main.py
+++ main.py
@@ -1,56 +1,66 @@
+import numpy as np
 import torch
-import os
-import torch.nn as nn
+from torch import nn
 import torch.nn.functional as F
-from tqdm import tqdm
-from torchvision import models
+from torch import autograd
 from torchmeta.datasets import CIFARFS, Omniglot
 from torchmeta.transforms import Categorical, ClassSplitter
 from torchmeta.utils.data import BatchMetaDataLoader
 from torchvision.transforms import transforms
-from models import model_dict                
 from maml import MAML
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Using: {device}")
+if __name__ == '__main__':
+    test = False
+    num_way = 5 
+    shots = 1
+    train_max_samples = 5000
+    val_max_samples = 100
+    num_inner_steps = 1
+    inner_lr = 0.4
+    learn_inner_lrs = False
+    outer_lr = 0.001
+    maml = MAML(
+        num_way,
+        num_inner_steps,
+        inner_lr,
+        learn_inner_lrs,
+        outer_lr,
+        train_max_samples,
+        val_max_samples
+    )
+    train_transform = transforms.Compose([
+        transforms.Resize((32,32)),
+        # transforms.RandomCrop(32, padding=4),
+        # transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        # transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)),
+    ])
+    test_transform = transforms.Compose([
+        # transforms.Resize(256),
+        transforms.Resize((32,32)),
+        transforms.ToTensor(),
+        # transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)),
+    ])           
 
-train_transform = transforms.Compose([
-    transforms.Resize(256),
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    # transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)),
-])
-test_transform = transforms.Compose([
-    transforms.Resize(256),
-    # transforms.Resize((32,32)),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)),
-])           
+    train_dataset = Omniglot("data/", num_classes_per_task=num_way, meta_train=True, transform=train_transform, target_transform=Categorical(num_classes=num_way), download=True)
+    val_dataset = Omniglot("data/", num_classes_per_task=num_way, meta_val=True, transform=test_transform, target_transform=Categorical(num_classes=num_way), download=True)
+    test_dataset = Omniglot("data/", num_classes_per_task=num_way, meta_test=True, transform=test_transform, target_transform=Categorical(num_classes=num_way), download=True)
 
-CLASSES = 2
+    train_dataset = ClassSplitter(train_dataset, shuffle=True, num_train_per_class=1, num_test_per_class=15)
+    val_dataset = ClassSplitter(val_dataset, shuffle=True, num_train_per_class=1, num_test_per_class=15)
+    test_dataset = ClassSplitter(test_dataset, shuffle=True, num_train_per_class=1, num_test_per_class=15)
 
-train_dataset = CIFARFS("data/", num_classes_per_task=CLASSES, meta_train=True, transform=train_transform, target_transform=Categorical(num_classes=CLASSES), download=True)
-val_dataset = CIFARFS("data/", num_classes_per_task=CLASSES, meta_val=True, transform=test_transform, target_transform=Categorical(num_classes=CLASSES), download=True)
-test_dataset = CIFARFS("data/", num_classes_per_task=CLASSES, meta_test=True, transform=test_transform, target_transform=Categorical(num_classes=CLASSES), download=True)
+    train_loader = BatchMetaDataLoader(train_dataset, batch_size=32, num_workers=4)
+    val_loader = BatchMetaDataLoader(val_dataset, batch_size=32, num_workers=4)
+    test_loader = BatchMetaDataLoader(test_dataset, batch_size=32, num_workers=4)
 
-train_dataset = ClassSplitter(train_dataset, shuffle=True, num_train_per_class=16, num_test_per_class=16)
-val_dataset = ClassSplitter(val_dataset, shuffle=True, num_train_per_class=16, num_test_per_class=16)
-test_dataset = ClassSplitter(test_dataset, shuffle=True, num_train_per_class=16, num_test_per_class=16)
+    if test == False:
+        print("Training")
+        maml.train(
+            train_loader,
+            val_loader
+        )
+    else:
+        print("Testing")
+        maml.test(test_loader)
 
-train_loader = BatchMetaDataLoader(train_dataset, batch_size=16, num_workers=4)
-val_loader = BatchMetaDataLoader(val_dataset, batch_size=16, num_workers=4)
-test_loader = BatchMetaDataLoader(test_dataset, batch_size=16, num_workers=4)
-
-
-# model = model_dict['resnet110'](num_classes=3)
-# model = model.cuda()
-# model.fc = torch.nn.Linear(model.fc.in_features, 2)
-model = models.resnet18(num_classes=CLASSES)
-model = model.cuda()
-
-meta_optim = torch.optim.Adam(model.parameters(), lr=2e-4)
-
-maml = MAML(model, meta_optimizer=meta_optim, device=device, epochs=3, max_batches=100, tasks=16, num_adaption_steps=10)
-
-maml.outer_loop(train_loader, val_loader, test_loader)  
--- maml.py
+++ maml.py
@@ -1,100 +1,248 @@
-import copy 
-import torch
-from tqdm import tqdm
-import torch.nn.functional as F
-from collections import OrderedDict
+"""Implementation of model-agnostic meta-learning for Omniglot."""
 
+import argparse
+import os
+
+import numpy as np
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch import autograd
+from torchmeta.datasets import CIFARFS, Omniglot
+from torchmeta.transforms import Categorical, ClassSplitter
+from torchmeta.utils.data import BatchMetaDataLoader
+from torchvision.transforms import transforms
+
+NUM_INPUT_CHANNELS = 1
+NUM_HIDDEN_CHANNELS = 64
+KERNEL_SIZE = 3
+NUM_CONV_LAYERS = 4
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+SUMMARY_INTERVAL = 10
+SAVE_INTERVAL = 100
+LOG_INTERVAL = 10
+VAL_INTERVAL = LOG_INTERVAL * 5
+NUM_TEST_TASKS = 600
+
+def score(logits, labels):
+    """Returns the mean accuracy of a model's predictions on a set of examples.
+
+    Args:
+        logits (torch.Tensor): model predicted logits
+            shape (examples, classes)
+        labels (torch.Tensor): classification labels from 0 to num_classes - 1
+            shape (examples,)
+    """
+
+    assert logits.dim() == 2
+    assert labels.dim() == 1
+    assert logits.shape[0] == labels.shape[0]
+    y = torch.argmax(logits, dim=-1) == labels
+    y = y.type(torch.float)
+    return torch.mean(y).item()
 
 class MAML:
-    def __init__(self, model, meta_optimizer, device, epochs, max_batches, tasks, num_adaption_steps):
-        self.tasks = tasks
-        self.num_adaption_steps = num_adaption_steps
-        self.meta_model = model
-        self.device = device
-        self.meta_optimizer = meta_optimizer
-        self.epochs = epochs
-        self.max_batches = max_batches
-    
-    def inner_loop(self, train_data, train_labels, test_data, test_labels):
-        model = copy.deepcopy(self.meta_model)
-        model.to(self.device)
-        optim = torch.optim.SGD(model.parameters(), lr=0.05, momentum=0.9, weight_decay=5e-4)
+    """Trains and assesses a MAML."""
 
-        train_data, train_labels = train_data.to(self.device), train_labels.to(self.device)
-        test_data, test_labels = test_data.to(self.device), test_labels.to(self.device)
+    def __init__(
+            self,
+            num_outputs,
+            num_inner_steps,
+            inner_lr,
+            learn_inner_lrs,
+            outer_lr,
+            train_max_samples,
+            val_max_samples
+    ):
+        meta_parameters = {}
 
-        # Adapt model on query data
-        for i in range(self.num_adaption_steps):
-            outputs = model(train_data)
-            loss = F.cross_entropy(outputs, train_labels)
-            loss.backward()
-            optim.step()
-            optim.zero_grad()
+        # construct feature extractor
+        in_channels = NUM_INPUT_CHANNELS
+        for i in range(NUM_CONV_LAYERS):
+            meta_parameters[f'conv{i}'] = nn.init.xavier_uniform_(
+                torch.empty(
+                    NUM_HIDDEN_CHANNELS,
+                    in_channels,
+                    KERNEL_SIZE,
+                    KERNEL_SIZE,
+                    requires_grad=True,
+                    device=DEVICE
+                )
+            )
+            meta_parameters[f'b{i}'] = nn.init.zeros_(
+                torch.empty(
+                    NUM_HIDDEN_CHANNELS,
+                    requires_grad=True,
+                    device=DEVICE
+                )
+            )
+            in_channels = NUM_HIDDEN_CHANNELS
 
-        # Compute gradients on test data
-        test_outputs = model(test_data)
-        test_loss = F.cross_entropy(test_outputs, test_labels)
-        grads = torch.autograd.grad(test_loss, model.parameters(), create_graph=True)
+        # construct linear head layer
+        meta_parameters[f'w{NUM_CONV_LAYERS}'] = nn.init.xavier_uniform_(
+            torch.empty(
+                num_outputs,
+                NUM_HIDDEN_CHANNELS,
+                requires_grad=True,
+                device=DEVICE
+            )
+        )
+        meta_parameters[f'b{NUM_CONV_LAYERS}'] = nn.init.zeros_(
+            torch.empty(
+                num_outputs,
+                requires_grad=True,
+                device=DEVICE
+            )
+        )
 
-        # Acc
-        _, preds = torch.max(test_outputs, 1)
-        total_correct = (preds == test_labels).sum().item()
-        acc = total_correct / len(test_labels)
+        self._meta_parameters = meta_parameters
+        self._num_inner_steps = num_inner_steps
+        self._inner_lrs = {
+            k: torch.tensor(inner_lr, requires_grad=learn_inner_lrs)
+            for k in self._meta_parameters.keys()
+        }
+        self._outer_lr = outer_lr
 
-        meta_params = list(self.meta_model.parameters())
-        updated_params = list(model.parameters())
+        self._optimizer = torch.optim.Adam(
+            list(self._meta_parameters.values()) +
+            list(self._inner_lrs.values()),
+            lr=self._outer_lr
+        )
 
-        gradients = []
-        for i, params in enumerate(model.parameters()):
-            gradients.append(grads[i])
+        self.train_max_samples = train_max_samples
+        self.val_max_samples = val_max_samples
+        self._start_train_step = 0
 
+    def _forward(self, images, parameters):
+        x = images
+        for i in range(NUM_CONV_LAYERS):
+            x = F.conv2d(
+                input=x,
+                weight=parameters[f'conv{i}'],
+                bias=parameters[f'b{i}'],
+                stride=1,
+                padding='same'
+            )
+            x = F.batch_norm(x, None, None, training=True)
+            x = F.relu(x)
+        x = torch.mean(x, dim=[2, 3])
+        return F.linear(
+            input=x,
+            weight=parameters[f'w{NUM_CONV_LAYERS}'],
+            bias=parameters[f'b{NUM_CONV_LAYERS}']
+        )
 
-        # updated_params = OrderedDict(model.named_parameters())
-        # name_list, tensor_list = zip(*updated_params.items())
-        # for name, param, grad in zip(name_list, tensor_list, grads):
-        #     updated_params[name] = param - 0.5 * grad
+    def _inner_loop(self, images, labels, train):
+        accuracies = []
+        parameters = {
+            k: torch.clone(v)
+            for k, v in self._meta_parameters.items()
+        }
+        for _ in range(self._num_inner_steps):
+            # Forward propagation to obtain y_support
+            y_support = self._forward(images, parameters)
+            # Compute classification losses
+            inner_loss = F.cross_entropy(y_support, labels)
 
-        return gradients, acc, loss
+            # Compute accuracies
+            accuracies.append(score(y_support, labels))
 
-    
-    def outer_loop(self, train_loader, val_loader, test_loader):
-        for epoch in range(self.epochs):
-            for j, batch in enumerate(train_loader):
-                train_data, train_labels = batch['train']
-                test_data, test_labels = batch['test']
-                
-                gradients = []
-                accs = []
+            grads_list = autograd.grad(inner_loss, parameters.values(), create_graph=train)
+
+            for (i, n) in enumerate(parameters.keys()):
+                parameters[n] = parameters[n] - self._inner_lrs[n] * grads_list[i]
+
+        y_support = self._forward(images, parameters)
+        accuracies.append(score(y_support, labels))
+
+        return parameters, accuracies
+
+    def _outer_step(self, task_batch, train):
+        outer_loss_batch = []
+        accuracies_support_batch = []
+        accuracy_query_batch = []
+        batch_images_support, batch_labels_support = task_batch['train']
+        batch_images_query, batch_labels_query = task_batch['test']
+        for task in range(len(task_batch['train'][0])):
+            images_support, labels_support = batch_images_support[task], batch_labels_support[task]
+            images_query, labels_query = batch_images_query[task], batch_labels_query[task]
+
+            images_support = images_support.to(DEVICE)
+            labels_support = labels_support.to(DEVICE)
+            images_query = images_query.to(DEVICE)
+            labels_query = labels_query.to(DEVICE)
+
+            adapted_parameters, accuracy_support = self._inner_loop(images_support, labels_support, train=train)
+            y_query = self._forward(images_query, adapted_parameters)
+
+            # Compute classification losses
+            outer_loss = F.cross_entropy(y_query, labels_query)
+            outer_loss_batch.append(outer_loss)
+
+            accuracies_support_batch.append(accuracy_support)
+            accuracy_query = score(y_query, labels_query)
+            accuracy_query_batch.append(accuracy_query)
+        outer_loss = torch.mean(torch.stack(outer_loss_batch))
+        accuracies_support = np.mean(
+            accuracies_support_batch,
+            axis=0
+        )
+        accuracy_query = np.mean(accuracy_query_batch)
+        return outer_loss, accuracies_support, accuracy_query
+
+    def train(self, dataloader_train, dataloader_val):
+        for i_step, task_batch in enumerate(dataloader_train, start=self._start_train_step):
+            self._optimizer.zero_grad()
+            outer_loss, accuracies_support, accuracy_query = (
+                self._outer_step(task_batch, train=True)
+            )
+            outer_loss.backward()
+            self._optimizer.step()
+
+            if i_step % VAL_INTERVAL == 0:
+                print("Validating")
                 losses = []
-                for task in range(self.tasks):
-                    if task == 0:
-                        grads, acc, loss = self.inner_loop(train_data[task], train_labels[task], test_data[task], test_labels[task])
-                        for i in range(len(grads)):
-                            gradients.append(grads[i])
-                        accs.append(acc)
-                        losses.append(loss)
-                    else:
-                        grads, acc, loss = self.inner_loop(train_data[task], train_labels[task], test_data[task], test_labels[task])
-                        for i in range(len(grads)):
-                            gradients[i] += grads[i]
-                        accs.append(acc)
-                        losses.append(loss)
-                
-                # print(f"Epoch: {epoch}, Batch: {j}, Accuracy: {(sum(accs)/len(accs)):.3f} Loss: {(sum(losses)/len(losses)):.4f}")
+                accuracies_pre_adapt_support = []
+                accuracies_post_adapt_support = []
+                accuracies_post_adapt_query = []
+                for i, val_task_batch in enumerate(dataloader_val):
+                    outer_loss, accuracies_support, accuracy_query = (
+                        self._outer_step(val_task_batch, train=False)
+                    )
+                    losses.append(outer_loss.item())
+                    accuracies_pre_adapt_support.append(accuracies_support[0])
+                    accuracies_post_adapt_support.append(accuracies_support[-1])
+                    accuracies_post_adapt_query.append(accuracy_query)
 
-                # Average gradients
-                avg_gradients = []
-                for i, grad in enumerate(gradients):
-                    avg_gradients.append(grad / self.tasks)
+                    if i == self.val_max_samples:
+                        break
+                    
+                loss = np.mean(losses)
+                accuracy_pre_adapt_support = np.mean(
+                    accuracies_pre_adapt_support
+                )
+                accuracy_post_adapt_support = np.mean(
+                    accuracies_post_adapt_support
+                )
+                accuracy_post_adapt_query = np.mean(
+                    accuracies_post_adapt_query
+                )
 
-                # Update meta model
-                for i, params in enumerate(self.meta_model.parameters()):
-                    params.grad = avg_gradients[i]
-
-                self.meta_optimizer.step()
-                self.meta_optimizer.zero_grad()
-                
-                if j == self.max_batches:
-                    break   
+                print(f"Loss: {loss}, acc_pre_adp_sup: {accuracy_pre_adapt_support}, acc_post_adp_sup: {accuracy_post_adapt_support}, acc_post_adp_query: {accuracy_post_adapt_query}") 
+            if i_step == self.train_max_samples:
+                break
 
 
+    def test(self, dataloader_test):
+        accuracies = []
+        for task_batch in dataloader_test:
+            _, _, accuracy_query = self._outer_step(task_batch, train=False)
+            accuracies.append(accuracy_query)
+        mean = np.mean(accuracies)
+        std = np.std(accuracies)
+        mean_95_confidence_interval = 1.96 * std / np.sqrt(NUM_TEST_TASKS)
+        print(
+            f'Accuracy over {NUM_TEST_TASKS} test tasks: '
+            f'mean {mean:.3f}, '
+            f'95% confidence interval {mean_95_confidence_interval:.3f}'
+        )
