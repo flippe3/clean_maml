9aa6543cad6543f5737f009c3b862d5fc35d11b0
Filip Nilsson <filip.c.nilsson@gmail.com>
2023-05-22 23:54:25 +0200

    cleanup

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ from torchmeta.datasets import CIFARFS, Omniglot, MiniImagenet
 from torchmeta.transforms import Categorical, ClassSplitter
 from torchmeta.utils.data import BatchMetaDataLoader
 from torchvision.transforms import transforms
-from maml import MAML
 
 if __name__ == '__main__':
     test = False
-    num_way = 5 
+    n_way = 5 
     shots = 1
     train_max_samples = 5000
     val_max_samples = 100
@@ -20,36 +19,27 @@ if __name__ == '__main__':
     learn_inner_lrs = False
     outer_lr = 0.001
 
-    maml = MAML(
-        num_way,
-        num_inner_steps,
-        inner_lr,
-        learn_inner_lrs,
-        outer_lr,
-        train_max_samples,
-        val_max_samples
-    )
-
     transform = transforms.Compose([
         transforms.Resize((32,32)),
         # transforms.Grayscale(num_output_channels=1),
         transforms.ToTensor(),
     ])
 
-    train_dataset = MiniImagenet("data/", num_classes_per_task=num_way, meta_train=True, transform=transform, target_transform=Categorical(num_classes=num_way), download=True)
-    val_dataset = MiniImagenet("data/", num_classes_per_task=num_way, meta_val=True, transform=transform, target_transform=Categorical(num_classes=num_way), download=True)
-    test_dataset = MiniImagenet("data/", num_classes_per_task=num_way, meta_test=True, transform=transform, target_transform=Categorical(num_classes=num_way), download=True)
+    train_dataset = Omniglot("data/", num_classes_per_task=n_way, meta_train=True, transform=transform, target_transform=Categorical(num_classes=n_way), download=False)
+    val_dataset = Omniglot("data/", num_classes_per_task=n_way, meta_val=True, transform=transform, target_transform=Categorical(num_classes=n_way), download=False)
+    test_dataset = Omniglot("data/", num_classes_per_task=n_way, meta_test=True, transform=transform, target_transform=Categorical(num_classes=n_way), download=False)
 
-    train_dataset = ClassSplitter(train_dataset, shuffle=True, num_train_per_class=1, num_test_per_class=15)
-    val_dataset = ClassSplitter(val_dataset, shuffle=True, num_train_per_class=1, num_test_per_class=15)
-    test_dataset = ClassSplitter(test_dataset, shuffle=True, num_train_per_class=1, num_test_per_class=15)
+    train_dataset = ClassSplitter(train_dataset, shuffle=True, num_train_per_class=1, num_test_per_class=shots)
+    val_dataset = ClassSplitter(val_dataset, shuffle=True, num_train_per_class=1, num_test_per_class=shots)
+    test_dataset = ClassSplitter(test_dataset, shuffle=True, num_train_per_class=1, num_test_per_class=shots)
 
     train_loader = BatchMetaDataLoader(train_dataset, batch_size=32, num_workers=4)
     val_loader = BatchMetaDataLoader(val_dataset, batch_size=32, num_workers=4)
     test_loader = BatchMetaDataLoader(test_dataset, batch_size=32, num_workers=4)
 
-    if test == False:
-        maml.train(train_loader,val_loader)
-    else:
-        maml.test(test_loader)
+    for i, batch in enumerate(train_loader):
+        x_sup, y_sup = batch['train']
+        x_qry, y_qry = batch['test']
+        break
+        
 
---
+++ maml_parallel.py
@@ -0,0 +1,170 @@
+from support.omniglot_loaders import OmniglotNShot
+from torch.func import vmap, grad, functional_call
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision
+from torch import nn
+import torch
+import matplotlib.pyplot as plt
+import argparse
+import time
+import functools
+import torchvision
+from functools import partial
+import wandb
+
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+plt.style.use('bmh')
+
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="5"
+
+run = wandb.init(
+  mode="disabled",
+  project="meta-kd",
+  dir="./",
+  name = 'resnet18-omniglot-5-1',
+  tags=["omniglot", "resnet101", "maml", "functional"],
+)
+class MAML:
+    # Trains a model for n_inner_iter using the support and returns a loss
+    # using the query.
+    def loss_for_task(self, net, n_inner_iter, x_spt, y_spt, x_qry, y_qry):
+        params = dict(net.named_parameters())
+        buffers = dict(net.named_buffers())
+        querysz = x_qry.size(0)
+
+        def compute_loss(new_params, buffers, x, y):
+            logits = functional_call(net, (new_params, buffers), x)
+            loss = F.cross_entropy(logits, y)
+            return loss
+
+        new_params = params
+        for _ in range(n_inner_iter):
+            grads = grad(compute_loss)(new_params, buffers, x_spt, y_spt)
+            new_params = {k: new_params[k] - g * 1e-1 for k, g, in grads.items()}
+
+        # The final set of adapted parameters will induce some
+        # final loss and accuracy on the query dataset.
+        # These will be used to update the model's meta-parameters.
+        qry_logits = functional_call(net, (new_params, buffers), x_qry)
+        qry_loss = F.cross_entropy(qry_logits, y_qry)
+        qry_acc = (qry_logits.argmax(dim=1) == y_qry).sum() / querysz
+
+        return qry_loss, qry_acc
+
+
+    def train(self, db, net, device, meta_opt, epoch, num_adaption_steps):
+        params = dict(net.named_parameters())
+        buffers = dict(net.named_buffers())
+        n_train_iter = db.x_train.shape[0] // db.batchsz
+        
+        for batch_idx in range(n_train_iter):
+            start_time = time.time()
+            # Sample a batch of support and query images and labels.
+            x_spt, y_spt, x_qry, y_qry = db.next()
+
+            task_num, setsz, c_, h, w = x_spt.size()
+
+            meta_opt.zero_grad()
+
+            # In parallel, trains one model per task. There is a support (x, y)
+            # for each task and a query (x, y) for each task.
+            compute_loss_for_task = functools.partial(self.loss_for_task, net, num_adaption_steps)
+            qry_losses, qry_accs = vmap(compute_loss_for_task)(x_spt, y_spt, x_qry, y_qry)
+
+            # Compute the maml loss by summing together the returned losses.
+            qry_losses.sum().backward()
+
+            meta_opt.step()
+            qry_losses = qry_losses.detach().sum() / task_num
+            qry_accs = 100. * qry_accs.sum() / task_num
+            i = epoch + float(batch_idx) / n_train_iter
+            iter_time = time.time() - start_time
+            if batch_idx % 4 == 0:
+                print(f'[Epoch {i:.2f}] Train Loss: {qry_losses:.2f} | Acc: {qry_accs:.2f} | Time: {iter_time:.2f}')
+                wandb.log({'loss': qry_losses, 'acc': qry_accs, 'epoch': i})
+
+    def test(self, db, net, device, epoch, num_adaption_steps):
+        # Crucially in our testing procedure here, we do *not* fine-tune
+        # the model during testing for simplicity.
+        # Most research papers using MAML for this task do an extra
+        # stage of fine-tuning here that should be added if you are
+        # adapting this code for research.
+        params = dict(net.named_parameters())
+        buffers = dict(net.named_buffers())
+        n_test_iter = db.x_test.shape[0] // db.batchsz
+
+        qry_losses = []
+        qry_accs = []
+
+        for batch_idx in range(n_test_iter):
+            x_spt, y_spt, x_qry, y_qry = db.next('test')
+            task_num, setsz, c_, h, w = x_spt.size()
+
+            # TODO: Maybe pull this out into a separate module so it
+            # doesn't have to be duplicated between `train` and `test`?
+
+            for i in range(task_num):
+                new_params = params
+                for _ in range(num_adaption_steps):
+                    spt_logits = functional_call(net, (new_params, buffers), x_spt[i])
+                    spt_loss = F.cross_entropy(spt_logits, y_spt[i])
+                    grads = torch.autograd.grad(spt_loss, new_params.values())
+                    new_params = {k: new_params[k] - g * 1e-1 for k, g, in zip(new_params, grads)}
+
+                # The query loss and acc induced by these parameters.
+                qry_logits = functional_call(net, (new_params, buffers), x_qry[i]).detach()
+                qry_loss = F.cross_entropy(qry_logits, y_qry[i], reduction='none')
+                qry_losses.append(qry_loss.detach())
+                qry_accs.append((qry_logits.argmax(dim=1) == y_qry[i]).detach())
+
+        qry_losses = torch.cat(qry_losses).mean().item()
+        qry_accs = 100. * torch.cat(qry_accs).float().mean().item()
+        print(f'[Epoch {epoch+1:.2f}] Test Loss: {qry_losses:.2f} | Acc: {qry_accs:.2f}')
+        wandb.log({'test_loss': qry_losses, 'test_acc': qry_accs})
+
+
+if __name__ == '__main__':
+    epochs = 100
+    batch_size = 32
+    n_way, k_spt, k_qry = 5, 1, 1
+    num_adaption_steps = 1
+    seed = 42
+
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+
+    # Set up the Omniglot loader.
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    db = OmniglotNShot(
+        '/tmp/omniglot-data',
+        batchsz=batch_size,
+        n_way=n_way,
+        k_shot=k_spt,
+        k_query=k_qry,
+        imgsz=28,
+        device=device,
+    )
+
+    maml = MAML()
+
+    net = torchvision.models.resnet18(norm_layer=partial(nn.BatchNorm2d, track_running_stats=False))
+    net.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False) # Since omniglot is grayscale
+    net.fc = nn.Linear(net.fc.in_features, n_way)
+    net.to(device)
+    net.train()
+
+    meta_opt = optim.Adam(net.parameters(), lr=1e-3)
+
+    for epoch in range(epochs):
+        maml.train(db, net, device, meta_opt, epoch, num_adaption_steps)
+        maml.test(db, net, device, epoch, num_adaption_steps)
+    
+    torch.save(net.state_dict(), 'trained/resnet18-omniglot-5-1.pth')
